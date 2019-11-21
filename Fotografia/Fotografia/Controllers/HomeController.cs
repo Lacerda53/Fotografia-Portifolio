cbea609c8fa6cc51795ea6230f41fe2a40bc9f65
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2019-11-21 16:30:01 -0300

    Layout pagina principal

--- Fotografia/Fotografia/Controllers/HomeController.cs
+++ Fotografia/Fotografia/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fotografia.Controllers
 {
+    
     public class HomeController : Controller
     {
+        /*https://livedemo00.template-help.com/wt_64615_v1/ */
         public IActionResult Index()
         {
             return View();
