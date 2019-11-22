818d789bbe6d12f17eb55011b310a5becc68d140
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2019-11-22 11:04:03 -0300

    Adicionando View components ao Index e fazendo a lógica do rating

---
+++ Fotografia/Fotografia/Models/AvaliacaoCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fotografia.Models
+{
+    public class AvaliacaoCliente
+    {
+        public AvaliacaoCliente()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Titlo { get; set; }
+        public string NomeCliente { get; set; }
+        public string ImagemCliente { get; set; }
+        public string Comentario { get; set; }
+        public int Avaliacao { get; set; }
+    }
+}
---
+++ Fotografia/Fotografia/ViewComponents/AvaliacaoClienteViewComponent.cs
@@ -0,0 +1,45 @@
+﻿using Fotografia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fotografia.ViewComponents
+{
+    [ViewComponent(Name ="AvaliacaoCliente")]
+    public class AvaliacaoClienteViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<AvaliacaoCliente> avaliacoes = new List<AvaliacaoCliente>()
+            {
+                new AvaliacaoCliente
+                {
+                    NomeCliente = "Guilherme Andrade Lacerda",
+                    Titlo = "Simplesmente Perfeito",
+                    ImagemCliente = "https://livedemo00.template-help.com/wt_64615_v1/images/user-6-46x46.jpg",
+                    Comentario = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    Avaliacao = 5
+                },
+                new AvaliacaoCliente
+                {
+                    NomeCliente = "Guilherme Andrade Lacerda",
+                    Titlo = "Simplesmente Perfeito",
+                    ImagemCliente = "https://livedemo00.template-help.com/wt_64615_v1/images/user-6-46x46.jpg",
+                    Comentario = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    Avaliacao = 4
+                },
+                new AvaliacaoCliente
+                {
+                    NomeCliente = "Guilherme Andrade Lacerda",
+                    Titlo = "Simplesmente Perfeito",
+                    ImagemCliente = "https://livedemo00.template-help.com/wt_64615_v1/images/user-6-46x46.jpg",
+                    Comentario = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    Avaliacao = 3
+                }
+            };
+            return View(avaliacoes);
+        }
+    }
+}
